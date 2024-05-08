97aa96154027d757c0ff2a277a6bf3ea83f9ffba
VINAYVARMAGIT <169148572+VINAYVARMAGIT@users.noreply.github.com>
2024-05-08 09:08:30 +0530

    new file of mine

---
+++ src/simplejavacalculatorTest/Maybe_One.java
@@ -0,0 +1,11 @@
+package simplejavacalculatorTest;
+
+public class Maybe_One {
+    public static void main(String[] args) {
+        
+    
+    System.out.println("hello world");
+    
+    }
+
+}
